b3114b3d5992fb309d730aeed9290eea0c452792
Zheleznyi <olexanderzhelezniy@gmail.com>
2019-07-29 16:54:13 +0300

    static#6

---
+++ StaticClasses/3/Sort.cs
@@ -0,0 +1,28 @@
+﻿namespace StaticClasses
+{
+    internal static class Sort
+    {
+        //Selection sort, O(n^2)
+        public static void SortAscending(this int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int index = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[index] > array[j])
+                        index = j;
+                }
+                
+                Swap<int>(ref array[index], ref array[i]);
+            }
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ StaticClasses/4/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StaticClasses
+{
+    internal static class Calculator
+    {
+        public static double CalculateSum(this double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double CalculateDiff(this double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double CalculateMultiple(this double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double CalculateDivision(this double a, double b)
+        {
+            if (b == 0)
+                return Double.NaN;
+            else
+                return a / b;
+        }
+    }
+}
