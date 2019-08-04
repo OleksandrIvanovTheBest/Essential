3dbfb8e6af0abc3f890f435f73c0c4c779563451
OleksandrZheleznyi <34132323+OleksandrZheleznyi@users.noreply.github.com>
2019-08-04 21:33:24 +0300

    generics#10 (#11)
    * generics#10
    
    * generics#10 IEnumerable problem
    
    * generics#10 infinite loop bug was fixed

---
+++ Generics/2/MyDictionary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+
+namespace Generics
+{
+    internal class MyDictionary<TKey, TValue>
+    {
+        private readonly string[] _rusWords = null;
+        private readonly string[] _engWords = new string [5];
+
+        public int Count
+        {
+            get
+            {
+                return _rusWords.Length;
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Count)
+                    return _rusWords[index] + " - " + " - " + _engWords[index];
+                else
+                    return "Attempt to access outside the array.";
+            }
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < Count; i++)
+                {
+                    if (_rusWords[i] == index)
+                        return _rusWords[i] + " - " + _engWords[i];
+                    else if (_engWords[i] == index)
+                        return _engWords[i] + " - " + _rusWords[i];
+                }
+                return string.Format("{0} - no translation for this word.", index);
+            }
+        }
+
+        public MyDictionary()
+        {
+            _rusWords = new string[_engWords.Length];
+
+            _engWords[0] = "book";
+            _engWords[1] = "pen";
+            _engWords[2] = "sun";
+            _engWords[3] = "apple";
+            _engWords[4] = "table";
+
+            Array.ForEach(_engWords, EncodeString);
+        }
+
+        public void Add(int index, string item)
+        {
+            _rusWords[index - 1] = item;
+        }
+ 
+        private void EncodeString(string str)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            str = Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            MyList<int> myList1 = new MyList<int>();
+            Console.WriteLine(myList1.Count);
+
+            myList1.Add(7);
+            myList1.Add(9);
+            myList1.Add(0);
+            myList1.Add(3);
+
+            Show(myList1);
+
+            MyList<int> myList2 = new MyList<int>(new int[] { 1, 3, 4, 9 });
+            Show(myList2);
+
+            myList2.AddRange(new int[] { 8, 8, 8, 8 });
+            Show(myList2);
+
+            Console.WriteLine();
+            //2
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "книга");
+            myDictionary.Add(3, "солнце");
+            myDictionary.Add(4, "яблоко");
+            myDictionary.Add(2, "ручка");
+            myDictionary.Add(5, "стол");
+
+            Console.WriteLine(myDictionary.Count);
+
+            Console.WriteLine(myDictionary["книга"]);
+            Console.WriteLine(myDictionary["дом"]);
+            Console.WriteLine(myDictionary["ручка"]);
+            Console.WriteLine(myDictionary["стол"]);
+            Console.WriteLine(myDictionary["карандаш"]);
+            Console.WriteLine(myDictionary["яблуко"]);
+            Console.WriteLine(myDictionary["солнце"]);
+            Console.WriteLine(myDictionary["sun"]);
+            Console.WriteLine(myDictionary["table"]);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(myDictionary[i]);
+            }
+
+            Console.WriteLine();
+            //3
+            Array.ForEach(myList2.GetArray(), Console.Write);
+
+            Console.WriteLine();
+            //4
+            var aInt = MyClass<int>.FactoryMethod();
+            aInt = 10;
+
+            if (aInt.GetType() == typeof(int))
+            {
+                Console.WriteLine(aInt);
+            }
+
+            var bDouble = MyClass<double>.FactoryMethod();
+            bDouble = 13.894;
+
+            if (bDouble.GetType() == typeof(double))
+            {
+                Console.WriteLine(bDouble);
+            }
+        }
+
+        private static void Show(IEnumerable collection)
+        {
+            foreach (int item in collection)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
