bac2a94d6b1eb86a0f5362096d00ebde730247a6
Zheleznyi <olexanderzhelezniy@gmail.com>
2019-08-17 21:41:29 +0300

    collections#14

---
+++ Collections/2/MyDictionary.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    public class MyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private KeyValuePair<TKey, TValue>[] _keyValuePairs = null;
+
+        public int Count
+        {
+            get
+            {
+                return _keyValuePairs.Length;
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (ContainsKey(key))
+                {
+                    return _keyValuePairs[GetIndexKey(key)].Value;
+                }
+                throw new ArgumentOutOfRangeException("index parameter is out of range.");
+            }
+        }
+
+        public MyDictionary()
+        {
+            _keyValuePairs = new KeyValuePair<TKey, TValue>[0];
+        }
+
+        public MyDictionary(int size)
+        {
+            _keyValuePairs = new KeyValuePair<TKey, TValue>[size];
+        }
+
+        private bool ContainsKey(TKey key)
+        {
+            return GetIndexKey(key) == -1 ? false : true;
+        }
+
+        private int GetIndexKey(TKey key)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Equals(_keyValuePairs[i].Key, key))
+                    return i;
+            }
+            return -1;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            IncreaseArrayLength();
+            _keyValuePairs[Count - 1] = new KeyValuePair<TKey, TValue>(key, value);
+        }
+
+        private void IncreaseArrayLength(int item = 1)
+        {
+            Array.Resize(ref _keyValuePairs, Count + item);
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<TKey, TValue>>)_keyValuePairs).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+
+        //Or use yield
+        /*
+        int position = -1;
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < Count - 1)
+                {
+                    position++;
+                    yield return _keyValuePairs[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;  // Выход из цикла.       
+                }
+            }
+        }
+        */
+    }
+}
---
+++ Collections/3/Arrays.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    internal static class Arrays
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            IEnumerator ie = list.GetEnumerator();
+
+            int size = 0;
+
+            while (ie.MoveNext())
+                size++;
+
+            ie.Reset();
+
+            T[] array = new T[size];
+
+            for (int i = 0; i < size && ie.MoveNext(); i++)
+            {
+                array[i] = (T)ie.Current;
+            }
+
+            ie.Reset();
+
+            return array;
+        }
+    }
+}
