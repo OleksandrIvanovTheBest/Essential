80974c23a208aa9c5252f194b38b8ee734c2907d
OleksandrZheleznyi <34132323+OleksandrZheleznyi@users.noreply.github.com>
2019-08-19 01:28:15 +0300

    Linq
    * Linq#17
    
    * using the LINQ extension methods instead SQL syntax
    
    * fix linq

---
+++ Linq/1/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Linq
+{
+    class Customer
+    {
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            var cars = new List<Car>
+            {
+                new Car
+                {
+                    Mark = "Audi",
+                    Model = "R8",
+                    Year = 2018,
+                },
+                new Car
+                {
+                    Mark = "BMW",
+                    Model = "M5",
+                    Year = 2015,
+                }
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Model = "R8",
+                    Name = "Sasha",
+                    Phone = 0985556622,
+                },
+                new Customer
+                {
+                    Model = "M5",
+                    Name = "Aleksandr",
+                    Phone = 0936668885,
+                },
+            };
+
+            var query = customers.Join(cars,
+                                       cus => cus.Model,
+                                       car => car.Model,
+                                       (cus, car) => new
+                                       {
+                                           cus.Model,
+                                           cus.Name,
+                                           cus.Phone,
+                                           car.Mark,
+                                           car.Year
+                                       }).Where(cusName => cusName.Name == "Sasha");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("Model - {0}, name - {1}, phone - {2}, Mark - {3}, Year - {4}",
+                                    item.Model, item.Name, item.Phone, item.Mark, item.Year);
+            }
+
+            Console.WriteLine();
+            //2
+            var dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["книга"]);
+            Console.WriteLine(dictionary["дом"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стіл"]);
+            Console.WriteLine(dictionary["карандаш"]);
+            Console.WriteLine(dictionary["яблуко"]);
+            Console.WriteLine(dictionary["солнце"]);
+            Console.WriteLine(dictionary["sun"]);
+            Console.WriteLine(dictionary["table"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (dynamic i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            Console.WriteLine();
+            //3
+            dynamic calculator = new Calculator();
+
+            Console.WriteLine(calculator.Add(4, 3));
+            Console.WriteLine(calculator.Add("1", 2));
+            Console.WriteLine(calculator.Diff(10, 4));
+
+            Console.ReadLine();
+        }
+    }
+}
