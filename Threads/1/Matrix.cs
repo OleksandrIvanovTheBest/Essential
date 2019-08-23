e754ac07d2d9536ed5b32babb006305caced58b9
Zheleznyi <olexanderzhelezniy@gmail.com>
2019-08-23 22:39:10 +0300

    threads do it

---
+++ Threads/1/Matrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    internal class Matrix
+    {
+        object block = new object();
+
+        private void MessageCreate(int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(GetRandomChar());
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(GetRandomChar());
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(GetRandomChar());
+
+            Console.ResetColor();
+        }
+
+        public void MessageMove()
+        {
+            lock (block)
+            {
+                int height = GetHeight();
+
+                //for (int i = 0; i < Console.WindowHeight - height - 3; i++)
+                //{
+                    MessageCreate(height);
+                    Thread.Sleep(500);
+                    //Console.Clear();
+                    //Console.SetCursorPosition(0, i + 1);//??
+                //}
+            }
+        }
+
+        private static char GetRandomChar()
+        {
+            string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+            Random rand = new Random();
+            int num = rand.Next(0, chars.Length - 1);
+            return chars[num];
+        }
+
+        private static int GetHeight()
+        {
+            Random rand = new Random();
+            return rand.Next(15);
+        }
+    }
+}
--- Threads/Program.cs
+++ Threads/Program.cs
@@ -5,27 +5,87 @@ namespace Threads
 {
     class Program
     {
-        static readonly object locker = new object();
+        protected static int origRow;
+        protected static int origCol;
 
-        static void ViewMessage(object height)
+        delegate void Delegat();
+
+        static object block3 = new object();
+
+        public static void FuncMove()
         {
-            for (int i = 0; i < (int)height; i++)
+            lock (block3)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
+               
+                for (int i = 0; i < 8; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(GetRandomChar());
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(GetRandomChar());
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(GetRandomChar());
+
+                Console.ResetColor();
             }
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(GetRandomChar());
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(GetRandomChar());
-
-            Console.ResetColor();
         }
 
         static void Main(string[] args)
         {
             //1
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write(formatString, i);
+            //    Console.WriteLine();
+            //    //Console.WriteLine("{0,4}", i);
+            //}
+            //Console.ReadLine();
+
+            //Matrix matrix = new Matrix();
+
+            //ThreadStart matrix1 = new ThreadStart(matrix.MessageMove);
+            //Thread thread1 = new Thread(matrix1);
+            //thread1.Start();
+
+            //matrix.MessageMove();
+
+            Delegat delegat = new Delegat(FuncMove);
+
+            int origRow = Console.CursorTop;
+            int origCol = Console.CursorLeft;
+
+            WriteAt(delegat, 0, 0);
+            WriteAt(delegat, 1, 0);
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        //Console.SetCursorPosition(i, 0);
+            //        //Console.Write(" ");
+            //        //matrix.Method();
+            //        //Console.SetCursorPosition(i, j);
+            //        WriteAt(delegat, i, j);
+
+
+            //        //new Thread(matrix.Method).Start();
+            //        //Console.SetCursorPosition(i, i);
+            //        //Thread.Sleep(5000);
+            //    }
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    new Thread(matrix.MessageMove).Start();
+            //}
+
+            //Console.Clear();
+            //origRow = Console.CursorTop;
+            //origCol = Console.CursorLeft;
+
+            //Thread.Sleep(500);
             //ThreadStart matrixCode = new ThreadStart(MatrixCode);
             //Thread thread = new Thread(MatrixCode);
             //thread.Start();
@@ -41,49 +101,44 @@ namespace Threads
 
             //Console.SetWindowSize(80, 25);
 
-            int height = GetHeight();
+            //ParameterizedThreadStart viewMessage = new ParameterizedThreadStart(ViewMessage);
+            //Thread thread = new Thread(viewMessage);
+            //thread.Start(height);
 
-            ParameterizedThreadStart viewMessage = new ParameterizedThreadStart(ViewMessage);
-            Thread thread = new Thread(viewMessage);
-            thread.Start(height);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < Console.WindowHeight - 8; j++)
+            //    {
+            //        ParameterizedThreadStart viewMessage = new ParameterizedThreadStart(ViewMessage);
+            //        Thread thread = new Thread(viewMessage);
+            //        thread.Start(height);
 
-
-            for (int i = 0; i < Console.WindowHeight - 8; i++)
-            {
-                ViewMessage(height);
-                Thread.Sleep(500);
-                Console.Clear();
-                Console.SetCursorPosition(0, i + 1);
-                //Console.WriteLine();
-            }
+            //        //ViewMessage(height);
+            //        Thread.Sleep(500);
+            //        Console.Clear();
+            //        Console.SetCursorPosition(i, j + 1);
+            //        //Console.WriteLine();
+            //    }
+            //}
 
             //Console.SetCursorPosition(0, i);
 
             //int origRow = Console.CursorTop;
             //int origCol = Console.CursorLeft;
 
-
             //Console.WriteLine("BufferWidth : " + Console.BufferWidth);//80
             //Console.WriteLine("BufferHeight: " + Console.BufferHeight);//300
             //Console.WriteLine("WindowWidth : " + Console.WindowWidth);//80
             //Console.WriteLine("WindowHeight: " + Console.WindowHeight);//25
-
-
-
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    lock (locker)
-            //    {
-            //        Console.ForegroundColor = ConsoleColor.Green;
-            //        Console.WriteLine("Primary");
-            //        Console.ForegroundColor = ConsoleColor.Gray;
-            //        Thread.Sleep(100);
-            //    }
-            //}
-
             //2
             //3
             Console.ReadLine();
+        }
+
+        private static void WriteAt(Delegat dg, int x, int y)
+        {
+            Console.SetCursorPosition(origCol + x, origRow + y);
+            dg.Invoke();
         }
 
         private static char GetRandomChar()
@@ -92,12 +147,6 @@ namespace Threads
             Random rand = new Random();
             int num = rand.Next(0, chars.Length - 1);
             return chars[num];
-        }
-
-        private static int GetHeight()
-        {
-            Random rand = new Random();
-            return rand.Next(15);
         }
     }
 }
