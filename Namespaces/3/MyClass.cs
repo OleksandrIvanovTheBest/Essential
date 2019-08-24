2c8cd197c6e8d5673af3299ae2a3f78302eb5707
OleksandrZheleznyi <34132323+OleksandrZheleznyi@users.noreply.github.com>
2019-08-24 19:46:37 +0300

    namespaces
    * namespaces
    
    * namespaces Hotfix

---
+++ Namespaces/3/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    internal class MyClass
+    {
+        public void MyClassMethod()
+        {
+            Console.WriteLine("MyClassMethod from namespace MyNamespace");
+        }
+    }
+}
