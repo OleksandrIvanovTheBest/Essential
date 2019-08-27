a99247b1955fc30841cf00218d362aa7a42a3b1c
Zheleznyi <olexanderzhelezniy@gmail.com>
2019-08-27 18:43:33 +0300

    threads tash #3

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -7,13 +7,32 @@ namespace Threads
     {
         static void Main(string[] args)
         {
-            //1
+            //1 and 2
             var matrix = new Matrix();
 
             matrix.StartMatrix();
-            
+
             while (true)
             {
+            }
+            //need comment task "1 and 2" to run task 3
+            //3
+            //PrintNumbersByDescending(6);
+        }
+
+        static void PrintNumbersByDescending(int x)
+        {
+            int hash = Thread.CurrentThread.GetHashCode();
+
+            if (x == 0)
+            {
+                Console.WriteLine("thread {0} - number {1}", hash, x);
+            }
+            else
+            {
+                Console.WriteLine("thread {0} - number {1}", hash, x);
+                Thread.Sleep(500);
+                new Thread(() => { PrintNumbersByDescending(x-1); }).Start();
             }
         }
     }
