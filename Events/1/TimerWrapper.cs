977ad5bc36773d1d98774ecb0e3741ae7805dd75
Zheleznyi <olexanderzhelezniy@gmail.com>
2019-09-01 22:12:57 +0300

    events tasks

---
+++ Events/1/TimerWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace Events
+{
+    internal class TimerWrapper
+    {
+        private DispatcherTimer _timer;
+
+        public TimerWrapper(DispatcherTimer timer, EventHandler handler)
+        {
+            _timer = timer;
+            ConfigureTimer(handler, 1);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        private void ConfigureTimer(EventHandler handler, int interval)
+        {
+            _timer.Tick += handler;
+            _timer.Interval = TimeSpan.FromSeconds(interval);
+        }
+    }
+}
--- Events/MainWindow.xaml.cs
+++ Events/MainWindow.xaml.cs
@@ -1,28 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Events
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TimerWrapper _timer;
+
+        private int _secondsPassed;
+
+        private double _first, _second, result;
+
+        TextBox textBox;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _timer = new TimerWrapper(new DispatcherTimer(), TimerTick);
+        }
+        
+        //1
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void ButtonReset_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            _secondsPassed = 0;
+            LabelTimer.Content = "00:00:00";
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _secondsPassed++;
+            LabelTimer.Content = TimePassed();
+        }
+
+        private string TimePassed()
+        {
+            return TimeSpan.FromSeconds(_secondsPassed).ToString();
+        }
+
+        //2
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            result = _first + _second;
+            CalculationResult(result);
+        }
+
+        private void Sub_Click(object sender, RoutedEventArgs e)
+        {
+            result = _first - _second;
+            CalculationResult(result);
+        }
+
+        private void Mul_Click(object sender, RoutedEventArgs e)
+        {
+            result = _first * _second;
+            CalculationResult(result);
+        }
+
+        private void Div_Click(object sender, RoutedEventArgs e)
+        {
+            result = _first / _second;
+            CalculationResult(result);
+        }
+
+        private void FirstNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBox = sender as TextBox;
+            AssignmentOperands(textBox.Text, ref _first);
+        }
+
+        private void SecondNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBox = sender as TextBox;
+            AssignmentOperands(textBox.Text, ref _second);
+        }
+
+        private void CalculationResult(double result)
+        {
+            Result.Text = result.ToString();
+        }
+
+        private void AssignmentOperands(string input, ref double operand)
+        {
+            bool result = Double.TryParse(input, out double num);
+            if (result)
+            {
+                operand = num;
+            }
         }
     }
 }
