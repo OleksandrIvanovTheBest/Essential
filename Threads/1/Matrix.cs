467413384d4ee1a6453b655a8614e4226ee1aa37
OleksandrZheleznyi <34132323+OleksandrZheleznyi@users.noreply.github.com>
2019-09-06 22:30:08 +0300

    Threads (#21)
    * threads
    
    * threads do it
    
    * threads work
    
    * threads working together
    
    * task2 completed
    
    * working together for threads
    
    * threads tash #3
    
    * used Task insead Thread
    
    * problem Task solved

---
+++ Threads/1/Matrix.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Matrix
+    {
+        const string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+
+        private static int consoleWidth = Console.WindowWidth;
+        private static int consoleHeight = Console.WindowHeight;
+        private static object lockObject = new object();
+
+        private static Random rand = new Random();
+
+        public Matrix()
+        {
+            Console.CursorVisible = false;
+            ThreadPool.SetMaxThreads(10, 4);
+        }
+
+        public List<Task> StartMatrix()
+        {
+            List<Task> tasks = new List<Task>();
+
+            var shuffeledColumnNumbers = GetShuffeledColumnNumbers(consoleWidth);
+
+            foreach (var columnNumber in shuffeledColumnNumbers)
+            {
+                tasks.Add(Task.Run(() => MessageColumnMove(columnNumber, GetMessageHeight())));
+                tasks.Add(Task.Run(() => MessageColumnMove(columnNumber, GetMessageHeight())));
+            }
+            
+            return tasks;
+        }
+
+        private List<int> GetShuffeledColumnNumbers(int consoleWidth)
+        {
+            var shuffeledColumnNumbers = new List<int>();
+            var valuesQuantity = 0;
+
+            while (valuesQuantity < consoleWidth)
+            {
+                var currValue = rand.Next(consoleWidth);
+
+                if (!shuffeledColumnNumbers.Contains(currValue))
+                {
+                    shuffeledColumnNumbers.Add(currValue);
+                    valuesQuantity++;
+                }
+
+            }
+
+            return shuffeledColumnNumbers;
+        }
+
+        private static void MessageColumnMove(int columnNumber, int messageHeight)
+        {
+            int i = 0;
+
+            while (true)
+            {
+                PrintMessage(messageHeight, columnNumber, i, GetRandomChar);
+
+                Thread.Sleep(300);
+
+                PrintMessage(messageHeight, columnNumber, i, () => ' ');
+
+                i++;
+                if (i == consoleHeight - messageHeight)
+                {
+                    i = 0;
+                }
+            }
+        }
+
+        private static void PrintMessage(int messageHeight, int columnNumber, int rowNumber, Func<char> getChar)
+        {
+            for (int j = 0; j < messageHeight; j++)
+            {
+                lock (lockObject)
+                {
+                    SetCharColor(j, messageHeight);
+                    Console.SetCursorPosition(columnNumber, rowNumber + j);
+                    Console.Write(getChar());
+                }
+            }
+        }
+
+        private static void SetCharColor(int charPosition, int lastPosition)
+        {
+            if (charPosition == lastPosition - 1)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (charPosition == lastPosition - 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+            }
+        }
+
+        private static char GetRandomChar()
+        {
+            int num = rand.Next(0, chars.Length - 1);
+            return chars[num];
+        }
+
+        private static int GetMessageHeight()
+        {
+            return rand.Next(4, 10);
+        }
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 and 2
+            var matrix = new Matrix();
+
+            Task.WaitAll(matrix.StartMatrix().ToArray());
+
+            //need comment task "1 and 2" to run task 3
+            //3
+            //Task task = new Task(() => PrintNumbersByDescending(6));
+            //task.Start();
+
+            //Console.ReadLine();
+        }
+
+        static void PrintNumbersByDescending(int x)
+        {
+            if (x == 0)
+            {
+                Console.WriteLine("thread {0} - number {1}", Task.CurrentId, x);
+            }
+            else
+            {
+                Console.WriteLine("thread {0} - number {1}", Task.CurrentId, x);
+                new Task(() => Thread.Sleep(500));
+                new Task(() => { PrintNumbersByDescending(x - 1); }).Start();      
+            }
+        }
+    }
+}
